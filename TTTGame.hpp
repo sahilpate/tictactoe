18446eadb2654d46bd1ac7c2a16d4c84695fff95
Sahil <sahil@DESKTOP-N6UOC5J.localdomain>
2021-07-28 13:27:12 -0400

    Declare TTTGame class

---
+++ TTTGame.hpp
@@ -0,0 +1,23 @@
+#ifndef TTTGAME_HPP
+#define TTTGAME_HPP
+
+#include TTTPlayer.hpp
+
+class TTTGame
+{
+public:
+    TTTGame();
+
+    void playGame();
+    void resetGame();
+
+private:
+    enum Winner { UNFINISHED, P1, P2, TIE };
+
+    TTTPlayer player[4];
+    int player1, player2;
+
+    Winner checkWin();
+};
+
+#endif //TTTGAME_HPP
