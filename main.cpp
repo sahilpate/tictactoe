4cb8fda06b86576507837231cd4992866add3550
Sahil <sahil@DESKTOP-N6UOC5J.localdomain>
2021-08-13 11:16:04 -0400

    Fix logical errors and improve UI

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include "TTTGame.hpp"
+
+void printRefBoard();
 
 int main()
 {
@@ -7,10 +10,24 @@ int main()
     do
     {
         game.selectPlayers();
+
+        std::cout << std::endl << "Each number corresponds to the following positions:" << std::endl;
+        printRefBoard();
+        std::cout << std::endl;
+
         game.playGame();
         game.resetGame();
     }
     while (game.checkPlayAgain() == true);
 
     return 0;
+}
+
+void printRefBoard()
+{
+    for(int i = 1; i < 10; ++i)
+    {
+        std::cout << i << ' ';
+        if(i % 3 == 0) std::cout << std::endl;
+    }
 }
